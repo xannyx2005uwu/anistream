be317475a874d4abd7c176864b809ccafd52cf9d
xannyx2005uwu <alessandrobirca05@gmail.com>
2026-04-02 16:00:48 +0200

    Delete test_cf.py

--- test_cf.py
+++
@@ -1,5 +0,0 @@
-import cloudscraper
-scraper = cloudscraper.create_scraper()
-r = scraper.get("https://www.animeunity.so/archivio?title=naruto")
-print("Status:", r.status_code)
-print("Content sample:", r.text[:200])
